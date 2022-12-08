5562879c835e25f973e304b7cab5e35e3016450a
EwokLord <dannyemcclainjr@gmail.com>
2022-12-08 10:34:22 -0600

    Everything we have before 3rd comp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Tester.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+Tester, this is where we write and test all of our code,
+ALWAYS test and write new code here, no where else
+ */
+
+@TeleOp(name="Tester", group="Controlled")
+public class Tester extends LinearOpMode {
+    ElapsedTime runtime = new ElapsedTime();
+    //pre init
+    //motors
+    DcMotorEx FLmotor;
+    DcMotorEx FRmotor;
+    DcMotorEx BLmotor;
+    DcMotorEx BRmotor;
+    DcMotorEx Xrail;
+
+    //Servos
+    Servo gripperL;
+    Servo gripperR;
+
+    //speed variables
+    double TurnSpeed = .5;
+    double drivespeed = .7;
+
+    //servo positions
+
+    public void runOpMode() {
+
+        //getting motors
+        BLmotor = hardwareMap.get(DcMotorEx.class, "BLmotor");
+        FLmotor = hardwareMap.get(DcMotorEx.class, "FLmotor");
+        BRmotor = hardwareMap.get(DcMotorEx.class, "BRmotor");
+        FRmotor = hardwareMap.get(DcMotorEx.class, "FRmotor");
+        Xrail = hardwareMap.get(DcMotorEx.class, "Xrail");
+
+
+
+        BRmotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        FLmotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        BRmotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        FRmotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        Xrail.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        //motor directions
+        BLmotor.setDirection(DcMotorEx.Direction.REVERSE);
+        FLmotor.setDirection(DcMotorEx.Direction.REVERSE);
+        BRmotor.setDirection(DcMotorEx.Direction.REVERSE);
+        FRmotor.setDirection(DcMotorEx.Direction.REVERSE);
+        Xrail.setDirection(DcMotorEx.Direction.REVERSE);
+
+        //getting servos
+        gripperL = hardwareMap.get(Servo.class,"gripperL");
+        gripperR = hardwareMap.get(Servo.class,"gripperR");
+
+        //servo directions
+        gripperR.setDirection(Servo.Direction.FORWARD);
+        gripperL.setDirection(Servo.Direction.REVERSE);
+
+        //init
+        gripperL.setPosition(.9);
+        gripperR.setPosition(.9);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            if(gamepad2.a){
+                gripperL.setPosition(.1);
+                gripperR.setPosition(.1);
+            }
+            else{
+                gripperL.setPosition(.9);
+                gripperR.setPosition(.9);
+            }
+
+
+            Xrail.setPower(gamepad2.left_stick_y * .8);
+
+            //drive
+            BLmotor.setPower(((gamepad1.right_trigger - gamepad1.left_trigger) * TurnSpeed) + (gamepad1.left_stick_y + gamepad1.right_stick_x)*(drivespeed));
+            FLmotor.setPower(((gamepad1.right_trigger - gamepad1.left_trigger) * TurnSpeed) + (gamepad1.left_stick_y - gamepad1.right_stick_x)*(drivespeed));
+            BRmotor.setPower(((gamepad1.right_trigger - gamepad1.left_trigger) * TurnSpeed) - (gamepad1.left_stick_y - gamepad1.right_stick_x)*(drivespeed));
+            FRmotor.setPower(((gamepad1.right_trigger - gamepad1.left_trigger) * TurnSpeed) - (gamepad1.left_stick_y + gamepad1.right_stick_x)*(drivespeed));
+            //end of drive
+        }
+    }
+}
+
+
+
+
